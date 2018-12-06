d03c29c30e646ec050fe34db8cd8581db2d0173c
hoojo <hoojo@qq.com>
2018-12-06 15:42:01 +0800

    :sparkles: :bento: :tada:  Init add private data collection use JavaSDK example

---
+++ src/test/java/com/cnblogs/hoojo/fabric/sdk/privatedata/PrivateDataExample.java
@@ -0,0 +1,17 @@
+package com.cnblogs.hoojo.fabric.sdk.privatedata;
+
+/**
+ * private data collection use JavaSDK example
+ * 使用私有数据示例
+ * @author hoojo
+ * @createDate 2018年12月6日 下午3:36:01
+ * @file PrivateDataExample.java
+ * @package com.cnblogs.hoojo.fabric.sdk.privatedata
+ * @project fabric-sdk-integrate
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class PrivateDataExample {
+
+}
