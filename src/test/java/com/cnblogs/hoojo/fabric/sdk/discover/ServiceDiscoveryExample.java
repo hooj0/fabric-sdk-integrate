a2b47f876c2c421e5693138d70d6e992f60bb47d
hoojo <hoojo@qq.com>
2018-12-06 15:41:59 +0800

    :sparkles: :bento: :tada:  Init add service discovery example

---
+++ src/test/java/com/cnblogs/hoojo/fabric/sdk/discover/ServiceDiscoveryExample.java
@@ -0,0 +1,18 @@
+package com.cnblogs.hoojo.fabric.sdk.discover;
+
+/**
+ * service discovery example
+ * 服务发现示例
+ * 
+ * @author hoojo
+ * @createDate 2018年12月6日 下午3:36:47
+ * @file ServiceDiscoveryExample.java
+ * @package com.cnblogs.hoojo.fabric.sdk.discover
+ * @project fabric-sdk-integrate
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class ServiceDiscoveryExample {
+
+}
