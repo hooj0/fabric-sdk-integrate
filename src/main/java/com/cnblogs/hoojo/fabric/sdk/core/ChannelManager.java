22a23955c5491b1ee759786921e6c8b95944db21
hoojo <hoojo@qq.com>
2018-12-07 20:24:08 +0800

    :sparkles: :bento: :recycle:  Changed 通道管理服务

--- src/main/java/com/cnblogs/hoojo/fabric/sdk/core/ChannelManager.java
+++ src/main/java/com/cnblogs/hoojo/fabric/sdk/core/ChannelManager.java
@@ -5,6 +5,7 @@ import static java.lang.String.format;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
@@ -319,9 +320,9 @@ public class ChannelManager extends ApplicationLogging {
 			Peer peer = client.newPeer(peerName, grpcURL, peerProps);
 
 			PeerOptions options = PeerOptions.createPeerOptions();
-			if (!config.isRunningAgainstFabric10()) {
+			if (config.isFabricVersionAtOrAfter("1.3")) {
 				// 默认 所有角色
-				options.registerEventsForBlocks();
+				options.setPeerRoles(EnumSet.of(PeerRole.ENDORSING_PEER, PeerRole.LEDGER_QUERY, PeerRole.CHAINCODE_QUERY, PeerRole.EVENT_SOURCE));
 			} else {
 				// 除事件源外的所有角色
 				options.setPeerRoles(PeerRole.NO_EVENT_SOURCE);
