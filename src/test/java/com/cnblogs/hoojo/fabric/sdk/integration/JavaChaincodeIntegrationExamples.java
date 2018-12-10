32289150b025ee76e6447848adc2e45f111bc95c
hoojo <hoojo@qq.com>
2018-12-10 16:10:51 +0800

    :sparkles: :bento: :recycle:  Changed Java Chaincode Integration Example

--- src/test/java/com/cnblogs/hoojo/fabric/sdk/integration/JavaChaincodeIntegrationExamples.java
+++ src/test/java/com/cnblogs/hoojo/fabric/sdk/integration/JavaChaincodeIntegrationExamples.java
@@ -1,8 +1,14 @@
 package com.cnblogs.hoojo.fabric.sdk.integration;
 
+import java.nio.file.Paths;
+
 import org.hyperledger.fabric.sdk.ChaincodeID;
+import org.hyperledger.fabric.sdk.Channel;
 import org.hyperledger.fabric.sdk.TransactionRequest.Type;
 import org.junit.Test;
+
+import com.cnblogs.hoojo.fabric.sdk.entity.InstallEntity;
+import com.cnblogs.hoojo.fabric.sdk.model.Organization;
 
 /**
  * Java Chaincode Integration Example
@@ -17,6 +23,8 @@ import org.junit.Test;
  */
 public class JavaChaincodeIntegrationExamples extends GoChaincodeIntegrationExamples {
 
+	String CHAIN_CODE_FILEPATH = "javacc/sample1";
+	
 	{
 		CHAIN_CODE_LANG = Type.JAVA;
 		CHAIN_CODE_NAME = "example_cc_java";
@@ -29,7 +37,27 @@ public class JavaChaincodeIntegrationExamples extends GoChaincodeIntegrationExam
 
 	@Test
 	public void testInstallChaincode() {
-		super.testInstallChaincode();
+		try {
+			Channel channel = getChannel();
+			Organization org = config.getOrganization(orgName);
+			
+			InstallEntity chaincode = new InstallEntity(id, CHAIN_CODE_LANG);
+			chaincode.setChaincodeSourceFile(Paths.get(config.getCommonConfigRootPath(), CHAIN_CODE_FILEPATH).toFile());
+			
+			client.setUserContext(org.getPeerAdmin());
+			if (chaincodeManager.checkInstallChaincode(channel, chaincode.getChaincodeId())) {
+				System.out.println(String.format("通道  %s 已安装chaincode 无需重复安装：%s ", channel.getName(), chaincode.getChaincodeId()));
+				return;
+			}
+			
+			chaincodeManager.installChaincode(channel, chaincode);
+
+			if (!chaincodeManager.checkInstallChaincode(channel, chaincode.getChaincodeId())) {
+				throw new AssertionError("chaincode 1 没有安装");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
